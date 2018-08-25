c4131189fc2fe565b05bdb4a7baecca8ef9b3d01
lnash94 <sumudunissanke@gmail.com>
2018-08-25 13:09:48 +0530

    Add 2 functions fibonacci and gcd functions were coded

--- services/server.js
+++ services/server.js
@@ -11,4 +11,41 @@ const server = http.createServer((req, res) => {
 
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
+    if ((hostname=='localhost')&&(port=="9999")){
+        // *******
+    }
 });
+
+// function gcd 
+function findGCD(a,b) {
+    a = Math.abs(a);
+    b = Math.abs(b);
+    if (b > a) {var temp = a; a = b; b = temp;}
+    while (true) {
+        if (b == 0){
+             return a;
+        }else{
+            a %= b;
+        }
+        if (a == 0){
+            return b;
+        }
+        else{
+            b %= a;
+        }
+    }
+}
+
+// function fibbonachi
+function fibonacci(num){
+    var a = 1, b = 0, temp;
+  
+    while (num >= 0){
+      temp = a;
+      a = a + b;
+      b = temp;
+      num--;
+    }
+  
+    return b;
+  }
